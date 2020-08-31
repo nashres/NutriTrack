bdaccac2a683496ebff7d75391eb414bf79bc304
nzbadi <nisrine.zbadi@gmail.com>
2020-08-31 15:26:38 -0600

    Updated Auth0

--- components/logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function logo() {
-    return (
-        <div className="logo">
-          <h1>NutriTrack</h1>
-        </div>
-    )
-}
--- views/analysis.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function analysis() {
-    return (
-        <div className="analysisPage">
-            <span>Analysis</span>
-        </div>
-    )
-}
